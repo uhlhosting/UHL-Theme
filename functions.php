f536939f20cef00e6884344e6ea3a655daf66c53
Martin Oviedo <63004949+ndawebs@users.noreply.github.com>
2020-08-02 00:21:06 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function o2_setup() {
      * Make theme available for translation.
      * Translations can be filed in the /languages/ directory.
      */
-    /* Pinegrow generated Load Text Domain Begin */
+    
     load_theme_textdomain( 'o2', get_template_directory() . '/languages' );
     /* Pinegrow generated Load Text Domain End */
 
@@ -30,24 +30,7 @@ function o2_setup() {
         'social'  => __( 'Social Links Menu', 'o2' ),
     ) );
 
-/*
-     * Register custom menu locations
-     */
-    /* Pinegrow generated Register Menus Begin */
 
-    /* Pinegrow generated Register Menus End */
-    
-/*
-    * Set image sizes
-     */
-    /* Pinegrow generated Image sizes Begin */
-
-    /* Pinegrow generated Image sizes End */
-    
-    /*
-     * Switch default core markup for search form, comment form, and comments
-     * to output valid HTML5.
-     */
     add_theme_support( 'html5', array(
         'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
     ) );
@@ -78,19 +61,7 @@ function o2_init() {
     register_taxonomy_for_object_type( 'category', 'attachment' );
     register_taxonomy_for_object_type( 'post_tag', 'attachment' );
 
-    /*
-     * Register custom post types. You can also move this code to a plugin.
-     */
-    /* Pinegrow generated Custom Post Types Begin */
 
-    /* Pinegrow generated Custom Post Types End */
-    
-    /*
-     * Register custom taxonomies. You can also move this code to a plugin.
-     */
-    /* Pinegrow generated Taxonomies Begin */
-
-    /* Pinegrow generated Taxonomies End */
 
 }
 endif; // o2_setup
@@ -119,12 +90,7 @@ if ( ! function_exists( 'o2_widgets_init' ) ) :
 
 function o2_widgets_init() {
 
-    /*
-     * Register widget areas.
-     */
-    /* Pinegrow generated Register Sidebars Begin */
 
-    /* Pinegrow generated Register Sidebars End */
 }
 add_action( 'widgets_init', 'o2_widgets_init' );
 endif;// o2_widgets_init
@@ -136,9 +102,6 @@ if ( ! function_exists( 'o2_customize_register' ) ) :
 function o2_customize_register( $wp_customize ) {
     // Do stuff with $wp_customize, the WP_Customize_Manager object.
 
-    /* Pinegrow generated Customizer Controls Begin */
-
-    /* Pinegrow generated Customizer Controls End */
 
 }
 add_action( 'customize_register', 'o2_customize_register' );
@@ -148,11 +111,6 @@ endif;// o2_customize_register
 if ( ! function_exists( 'o2_enqueue_scripts' ) ) :
     function o2_enqueue_scripts() {
 
-        /* Pinegrow generated Enqueue Scripts Begin */
-
-    /* Pinegrow generated Enqueue Scripts End */
-
-        /* Pinegrow generated Enqueue Styles Begin */
 
     wp_deregister_style( 'o2-style' );
     wp_enqueue_style( 'o2-style', get_template_directory_uri() . '/css/style.css', false, null, 'all');
@@ -160,19 +118,11 @@ if ( ! function_exists( 'o2_enqueue_scripts' ) ) :
     wp_deregister_style( 'o2-style-1' );
     wp_enqueue_style( 'o2-style-1', get_bloginfo('stylesheet_url'), false, null, 'all');
 
-    /* Pinegrow generated Enqueue Styles End */
 
     }
     add_action( 'wp_enqueue_scripts', 'o2_enqueue_scripts' );
 endif;
 
 function pgwp_sanitize_placeholder($input) { return $input; }
-/*
- * Resource files included by Pinegrow.
- */
-/* Pinegrow generated Include Resources Begin */
-require_once "inc/custom.php";
-require_once "inc/wp_pg_helpers.php";
 
-    /* Pinegrow generated Include Resources End */
 ?>
