f5bb306b4bbd5dc2800c9a51a8847518c0ba7dd0
Martin Oviedo <63004949+ndawebs@users.noreply.github.com>
2020-08-09 17:49:59 +0200

    Update 9/8

--- functions.php
+++ functions.php
@@ -1,32 +1,5 @@
 <?php
 
-//disable the Gutemberg editor and go back to the classic editor
-
-// disable for individual posts
-add_filter('use_block_editor_for_post', '__return_false', 10);
-
-// disable at the custom post type level
-add_filter('use_block_editor_for_post_type', '__return_false', 10);
-
-// Enable support for SVG
-
-function cc_mime_types($mimes) {
-    $mimes['svg'] = 'image/svg+xml';
-    return $mimes;
-   }
-   add_filter('upload_mimes', 'cc_mime_types');
-
-
-//Register new menu
-
-function register_my_menu() {
-  register_nav_menu('header-menu',__( 'Header Menu' ));
-  register_nav_menu('footer-menu',__( 'Footer Menu' ));
-  register_nav_menu('user-menu',__( 'User Menu' ));
-}
-add_action( 'init', 'register_my_menu' );
-
-
 // Custom post types
 
 // add your code here
@@ -35,6 +8,14 @@ add_action( 'init', 'register_my_menu' );
 
 // add your code here
 
-    while (true) {
-        echo "ur dumb";
+// This function will load all the php files that are inside the inc folder inside the template.
+
+$ruta = get_template_directory() .'/inc';
+//echo $ruta;
+$files = glob($ruta . '/*.php');
+//print_r($files);
+foreach ($files as $file) {
+    if ($file != __FILE__) {
+        include_once($file);
     }
+}
